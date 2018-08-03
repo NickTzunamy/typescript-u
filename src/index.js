dd94c3edbf19c97806a09b5ebce1efb6660c9b94
Nicu Eremia <nicolae.eremia@codefactorygroup.com>
2018-08-03 10:01:40 +0300

    init commit ts

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var logMsg = function (msg) {
+    console.log(msg);
+};
+logMsg('welcome');
+document.write('go');
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { interval } from 'rxjs';
+import { map, filter, sample } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
